f78735ec68a654a9cf6b1b5911c3f8bd1ccb49ea
chrischank <chrischank97@gmail.com>
2023-01-11 21:56:52 +0100

    initial commit

---
+++ after/plugin/colours.lua
@@ -0,0 +1,8 @@
+function ColourMyPencils(colour)
+	colour = "terafox"
+	vim.cmd.colorscheme(colour)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none"} )
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"} )
+end
+
+ColourMyPencils()
