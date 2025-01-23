3e71b444b0875499129fea78a2ae3c97aeaeb4e6
chanakin <chrischank97@gmail.com>
2025-01-23 16:53:56 +0000

    feat: absolute line number for insert and relative for visual and normal

--- lua/chanakin_setup/set.lua
+++ lua/chanakin_setup/set.lua
@@ -2,7 +2,22 @@ vim.opt.guicursor = ""
 vim.opt.cursorline = false
 
 vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.wo.number = true
+
+-- Create autocommands for switching line number modes
+vim.api.nvim_create_autocmd('InsertEnter', {
+  callback = function()
+    vim.wo.relativenumber = false
+    vim.wo.number = true
+  end
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  callback = function()
+    vim.wo.relativenumber = true
+    vim.wo.number = true
+  end
+})
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
