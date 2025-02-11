f690b162919009f13ccdd1123a999deda8f304c4
chanakin <chrischank97@gmail.com>
2025-02-11 11:57:57 +0000

    fix: try lsp fig

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,27 +1,7 @@
-local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
-
--- Install these servers
-lsp.ensure_installed({
-  'ts_ls',
-  'eslint',
-  'lua_ls',
-})
-
--- Pass arguments to a language server
-lsp.configure('ts_ls', {
-  on_attach = function(client, bufnr)
-    print('hello ts_ls')
-  end,
+local lspconfig = require('lspconfig')
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
   settings = {
-    completions = {
-      completeFunctionCalls = true
-    }
-  }
-})
-
--- Configure lua language server for neovim
-lsp.nvim_workspace()
-
-lsp.setup()
+    ['rust-analyzer'] = {},
+  },
+}
